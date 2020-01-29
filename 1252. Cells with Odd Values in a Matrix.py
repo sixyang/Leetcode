4b5b0d63dcf77e8cd94d623991f53c071f432b8f
sixyang <wuzhenyang7@gmail.com>
2020-01-29 10:28:38 +0800

    Update 1252. Cells with Odd Values in a Matrix.py
    不是那样的，看来是列表生成的问题，我的初始化列表用的方式为[[0] * m] * n，这个是有问题的。

--- 1252. Cells with Odd Values in a Matrix.py
+++ 1252. Cells with Odd Values in a Matrix.py
@@ -1,8 +1,19 @@
 class Solution:
     def oddCells(self, n: int, m: int, indices: List[List[int]]) -> int:
-        import numpy as np
-        nums = np.zeros((n, m))
+        # import numpy as np
+        # nums = np.zeros((n, m))
+        # for i in indices:
+        #     nums[i[0]] += 1
+        #     nums[:, i[1]] += 1
+        # return int(np.sum(nums % 2))
+        nums = [[0 for j in range(m)] for i in range(n)]
         for i in indices:
-            nums[i[0]] += 1
-            nums[:, i[1]] += 1
-        return int(np.sum(nums % 2))
+            nums[i[0]] = [j+1 for j in nums[i[0]]]
+            for k in range(len(nums)):
+                nums[k][i[1]] += 1
+        count = 0
+        for i in nums:
+            for j in i:
+                if j & 1 == 1:
+                    count += 1
+        return count
