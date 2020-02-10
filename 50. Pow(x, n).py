9fd0a1b75eda6fa80b126ea759f63c3ab067debf
sixyang <wuzhenyang7@gmail.com>
2020-02-10 10:56:20 +0800

    Create 50. Pow(x, n).py
    现在在学习之中，学习算法的最大感触就是——自己就是个傻子。啥都不会，啥都绕不过来。
    唉，这道题目😔也是看着官方题解做出来的，不然自己根本想不到这种简化的方法，就是用着for循环将n个数都跑一遍，啧，从未辜负我，果然又超时了。这种出栈式的递归操作我果然还不会，多试试吧，之后就会熟练了。现在感受到了莫大的学习困难啊。(っ °Д °;)っ

---
+++ 50. Pow(x, n).py
@@ -0,0 +1,11 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        def recursion(n, ret):
+            if n == 0:return 1                  #递归终止条件一定要写在最前面
+            half = recursion(n//2, ret)         #先一步到胃，这一步是核心，也是男点 ( •̀ ω •́ )y
+
+            #如果为偶数，就直接乘以一半的值，否则，结果为x**n-1，需要再乘以一个x
+            if n % 2 == 0:return half * half
+            else:return half * half * ret
+        if n < 0:return 1/recursion(abs(n), x)
+        else:return recursion(n, x)
